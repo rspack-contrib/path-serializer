c61a331ca8a36b1c8692fe85bdbbccfdd7b988b2
sunyiteng <sunyiteng@bytedance.com>
2024-10-16 17:13:55 +0800

    test(e2e): add e2e tests

---
+++ e2e/index.test.ts
@@ -0,0 +1,55 @@
+import { createRequire } from 'node:module';
+import path from 'node:path';
+import { expect, test } from 'vitest';
+import { createSnapshotSerializer } from '../dist/esm/main';
+
+const require = createRequire(import.meta.url);
+
+expect.addSnapshotSerializer(
+  createSnapshotSerializer({
+    cwd: path.resolve(__dirname, '..'),
+    workspace: path.resolve(__dirname, '..'),
+  }),
+);
+
+test('should serialize <ROOT>', () => {
+  const root = new URL('.', import.meta.url).href;
+
+  expect(root).toMatchInlineSnapshot(`"file://<ROOT>/e2e/"`);
+});
+
+test('should serialize <PNPM_INNER>', () => {
+  expect(require.resolve('@rslib/core')).toMatchInlineSnapshot(
+    `"<ROOT>/node_modules/<PNPM_INNER>/@rslib/core/dist/index.js"`,
+  );
+});
+
+test('should serialize Object', () => {
+  class MyObj {
+    attr: string;
+    constructor(attr: string) {
+      this.attr = attr;
+    }
+  }
+
+  const obj = new MyObj(require.resolve('@rslib/core'));
+  expect(obj).toMatchInlineSnapshot(`
+    MyObj {
+      "attr": "<ROOT>/node_modules/<PNPM_INNER>/@rslib/core/dist/index.js",
+    }
+  `);
+});
+
+test('should serialize file content', () => {
+  const fileContent = `"use strict";
+  const a = ${require.resolve('@rslib/core')};
+`;
+
+  expect(fileContent).toMatchInlineSnapshot(`
+    "\\"use strict\\";
+      const a = <ROOT>/node_modules/<PNPM_INNER>/@rslib/core/dist/index.js;
+    "
+  `);
+
+  expect(fileContent).toMatchSnapshot();
+});
