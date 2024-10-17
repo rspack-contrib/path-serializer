7519df9ced3d9207ff1e00a936b9b57f261955e7
Soon <sunyiteng@bytedance.com>
2024-10-17 16:00:51 +0800

    chore: support transform <PNPM_INNER> in win 32 (#4)

---
+++ e2e/pnpmInnerWin32.test.ts
@@ -0,0 +1,51 @@
+import path from 'node:path';
+import { expect, test } from 'vitest';
+import { createSnapshotSerializer } from '../dist/esm/index';
+
+test('should serialize <PNPM_INNER> in win32', () => {
+  const fileContent = `{
+      loader: 'D:\\Users\\user\\Documents\\code\\user\\fe-dev-scripts\\node_modules\\.pnpm\\css-loader@6.11.0_@rspack+core@packages+rspack_webpack@5.94.0_@swc+core@1.4.0_@swc+helpers@0._jlcdgjlw2ezzhg43ml3d627wdu\\node_modules\\css-loader\\utils.ts',
+  }`;
+
+  expect.addSnapshotSerializer(
+    createSnapshotSerializer({
+      cwd: path.resolve(__dirname, '..'),
+      workspace: path.resolve(__dirname, '..'),
+      features: {
+        ansiDoubleQuotes: false,
+        addDoubleQuotes: false,
+        transformWin32Path: false,
+      },
+    }),
+  );
+
+  expect(fileContent).toMatchInlineSnapshot(`
+    {
+          loader: 'D:\\Users\\user\\Documents\\code\\user\\fe-dev-scripts\\node_modules\\<PNPM_INNER>\\css-loader\\utils.ts',
+      }
+  `);
+});
+
+test('should serialize <PNPM_INNER> in win32 with transform', () => {
+  const fileContent = `{
+      loader: 'D:\\Users\\user\\Documents\\code\\user\\fe-dev-scripts\\node_modules\\.pnpm\\css-loader@6.11.0_@rspack+core@packages+rspack_webpack@5.94.0_@swc+core@1.4.0_@swc+helpers@0._jlcdgjlw2ezzhg43ml3d627wdu\\node_modules\\css-loader\\utils.ts',
+  }`;
+
+  expect.addSnapshotSerializer(
+    createSnapshotSerializer({
+      cwd: path.resolve(__dirname, '..'),
+      workspace: path.resolve(__dirname, '..'),
+      features: {
+        ansiDoubleQuotes: false,
+        addDoubleQuotes: false,
+        transformWin32Path: true,
+      },
+    }),
+  );
+
+  expect(fileContent).toMatchInlineSnapshot(`
+    {
+          loader: '/d/Users/user/Documents/code/user/fe-dev-scripts/node_modules/<PNPM_INNER>/css-loader/utils.ts',
+      }
+  `);
+});
